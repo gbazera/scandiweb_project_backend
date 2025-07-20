150c882b2dc41cbf2a5e1806ecc9aa91f4228d1d
Giorgi Bazerashvili <bazerashvili.gio27@gmail.com>
2025-07-20 05:22:18 +0400

    minor cors fix

--- public/index.php
+++ public/index.php
@@ -4,10 +4,12 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 
-if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header("Access-Control-Allow-Headers: Content-Type, Authorization");
-    exit(0);
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit;
 }
 
 require_once __DIR__ . '/../vendor/autoload.php';
